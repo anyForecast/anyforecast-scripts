3a92814d52182e289d231440370523548c0db0c8
Ramón Amézquita <ramon.amez@kuona.ai>
2024-02-12 18:10:40 -0600

    Testing logging

--- anyforecast_scripts/seq2seq/train.py
+++ anyforecast_scripts/seq2seq/train.py
@@ -1,3 +1,6 @@
+import tempfile
+import os
+
 import click
 import mlflow
 import pandas as pd
@@ -35,33 +38,51 @@ def train(
     X[group_cols] = X[group_cols].astype("category")
     X[datetime] = pd.to_datetime(X[datetime])
 
-    with mlflow.start_run():
+    print("mlflow version:", mlflow.__version__)
 
-        preprocessor = make_preprocessor(group_cols, datetime, target, freq)
+    with tempfile.TemporaryDirectory() as tmp_dir, mlflow.start_run() as run:
 
-        estimator = Seq2Seq(
-            group_ids=group_cols,
-            time_idx=datetime,
-            target=target,
-            time_varying_known_reals=time_varying_known,
-            time_varying_unknown_reals=time_varying_unknown,
-            static_categoricals=static_categoricals,
-            static_reals=static_reals,
-            min_encoder_length=max_encoder_length // 2,
-            max_encoder_length=max_encoder_length,
-            min_prediction_length=1,
-            max_prediction_length=max_prediction_length,
-            device=device,
-            max_epochs=max_epochs,
-            verbose=verbose,
-        )
+         # Added this line
+        print('tracking uri:', mlflow.get_tracking_uri())
+        print('artifact uri:', mlflow.get_artifact_uri())
 
-        pipe = PreprocessorEstimatorPipeline(
-            preprocessor, estimator, inverse_steps=["datetime", "target"]
-        )
-        pipe.fit(X)
+        fname = 'sample.txt'
+        tmp_path = os.path.join(tmp_dir, fname)
 
-        mlflow.sklearn.log_model(pipe, "model")
+        # create a text file to log
+        with open(tmp_path, 'w') as f:
+            f.write("sample")
+
+
+        mlflow.log_param("p", 0)
+        mlflow.log_metric("m", 1)
+        mlflow.log_artifact(tmp_path)
+
+        #preprocessor = make_preprocessor(group_cols, datetime, target, freq)
+
+        #estimator = Seq2Seq(
+        #    group_ids=group_cols,
+        #    time_idx=datetime,
+        #    target=target,
+        #    time_varying_known_reals=time_varying_known,
+        #    time_varying_unknown_reals=time_varying_unknown,
+        #    static_categoricals=static_categoricals,
+        #    static_reals=static_reals,
+        #    min_encoder_length=max_encoder_length // 2,
+        #    max_encoder_length=max_encoder_length,
+        #    min_prediction_length=1,
+        #    max_prediction_length=max_prediction_length,
+        #    device=device,
+        #    max_epochs=max_epochs,
+        #    verbose=verbose,
+        #)
+
+        #pipe = PreprocessorEstimatorPipeline(
+        #    preprocessor, estimator, inverse_steps=["datetime", "target"]
+        #)
+        #pipe.fit(X)
+
+        #mlflow.sklearn.log_model(pipe, "model")
 
 
 if __name__ == "__main__":
